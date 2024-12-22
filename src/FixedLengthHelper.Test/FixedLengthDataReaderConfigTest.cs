f159f14240cf0d10c9e7576ca2f205f0bce59b15
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-12-22 20:31:51 +0900

    wip

--- src/FixedLengthHelper.Test/FixedLengthDataReaderConfigTest.cs
+++ src/FixedLengthHelper.Test/FixedLengthDataReaderConfigTest.cs
@@ -48,28 +48,18 @@ public static class FixedLengthDataReaderConfigTest
             idColumn.Name.Should().Be("Id");
             idColumn.OffsetBytes.Should().Be(0);
             idColumn.LengthBytes.Should().Be(5);
-            idColumn.TrimMode.Should().Be(TrimMode.TrimStart);
-            idColumn.TrimChars.Should().Equal(' ', '0', '1', '2', '3');
-            //idColumn.IsDBNull(string.Empty).Should().BeTrue();
-            //idColumn.IsDBNull("abc").Should().BeFalse();
 
             var nameColumn = config.Columns[1];
             nameColumn.Ordinal.Should().Be(1);
             nameColumn.Name.Should().Be("Name");
             nameColumn.OffsetBytes.Should().Be(5);
             nameColumn.LengthBytes.Should().Be(10);
-            nameColumn.TrimMode.Should().Be(TrimMode.TrimEnd);
-            nameColumn.TrimChars.Should().BeNull();
-            //nameColumn.IsDBNull(string.Empty).Should().BeFalse();
 
             var descriptionColumn = config.Columns[2];
             descriptionColumn.Ordinal.Should().Be(2);
             descriptionColumn.Name.Should().Be("Description");
             descriptionColumn.OffsetBytes.Should().Be(15);
             descriptionColumn.LengthBytes.Should().Be(20);
-            descriptionColumn.TrimMode.Should().Be(TrimMode.Trim);
-            //descriptionColumn.TrimChars.Should().BeNull();
-            //descriptionColumn.IsDBNull(" ").Should().BeFalse();
         }
 
         [Fact]
--- src/FixedLengthHelper/Column.cs
+++ src/FixedLengthHelper/Column.cs
@@ -5,13 +5,33 @@ namespace FixedLengthHelper;
 /// <summary>
 /// Column definition.
 /// </summary>
-public record Column(
-    int Ordinal,
-    string Name,
-    int OffsetBytes,
-    int LengthBytes,
-    SqlDbType? SqlDbType,
-    TrimMode TrimMode,
-    char[]? TrimChars,
-    bool EmptyIsNull,
-    Func<string, object> Convert);
+public class Column
+{
+    private readonly Func<string, object> _convert;
+    /// <summary>
+    /// Column definition.
+    /// </summary>
+    public Column(
+        int ordinal,
+        string name,
+        int offsetBytes,
+        int lengthBytes,
+        SqlDbType? sqlDbType,
+        TrimMode trimMode,
+        char[]? trimChars,
+        bool emptyIsNull,
+        Func<string, object> convert)
+    {
+        Ordinal = ordinal;
+        Name = name;
+        OffsetBytes = offsetBytes;
+        LengthBytes = lengthBytes;
+        _convert = convert;
+    }
+    public int Ordinal { get; }
+    public string Name { get; }
+    public int OffsetBytes { get; }
+    public int LengthBytes { get; }
+
+    public object Convert(string value) => _convert(value);
+}
