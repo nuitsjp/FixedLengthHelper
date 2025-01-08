73d1ae6e3bb53b093791cbe4c2e9e8a9a06203f9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-08 10:35:45 +0900

    StringExtensionsクラスとメソッドの追加
    - `StringExtensions`クラスに`GetBytes`メソッドの追加
      - 指定されたエンコーディングを使用して文字列をバイト配列に変換し、そのバイト数を返す
    - `StringExtensions`クラスに`ToStream`メソッドの追加
      - 指定されたエンコーディングを使用して文字列をストリームに変換し、そのストリームを返す

---
+++ src/FixedLengthHelper.Test/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace FixedLengthHelper.Test;
+
+public static class StringExtensions
+{
+    public static int GetBytes(this string s, Encoding encoding)
+    {
+        var bytes = encoding.GetBytes(s);
+        return encoding.GetByteCount(s);
+    }
+
+    public static Stream ToStream(this string s, Encoding encoding)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream, encoding);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+}
